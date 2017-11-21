bfbc4f7d0856c21999d464c9551a726e61bdc07f
Martin Lautenbacher <lautenbacher.martin@gmail.com>
2017-11-21 16:47:46 +0100

    created files

---
+++ create_dataset.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+"""
+Creates train and test sets from the FlickrLogos-47 dataset
+
+
+__author__ = "Martin Lautenbacher"
+__version__ = "0.1"
+"""
+
+import image_functions as imf
+
---
+++ image_functions.py
@@ -0,0 +1,7 @@
+"""
+Functions to load, crop and scale images
+
+
+__author__ = "Martin Lautenbacher"
+__version__ = "0.1"
+"""
